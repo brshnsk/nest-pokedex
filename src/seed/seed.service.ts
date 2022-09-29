638598dd6a31ed284ec5ffe77c164cad13df5065
brshnsk <brshnsk@gmail.com>
2022-09-29 18:26:41 -0400

    Agregando insertar rápido multiples registros

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, {AxiosInstance} from 'axios'
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interfance';
 @Injectable()
 export class SeedService {
   
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
+  
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
-    const {data} =  await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=155')
 
-    data.results.forEach(({name, url})=>{
+    await this.pokemonModel.deleteMany({})
+    const {data} =  await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+
+    const pokemonToInsert: {name: string, no: number}[] = [];
+
+    data.results.forEach(async({name, url})=>{
       const segments = url.split('/');
       const no:number = +segments [segments.length -2];
-      console.log(name, no);
+      
+      pokemonToInsert.push({name,no})
     })
 
-    return data
+    await this.pokemonModel.insertMany(pokemonToInsert)
+
+    return 'Seed Executed'
   }
 }
