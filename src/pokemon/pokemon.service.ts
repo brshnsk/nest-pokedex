fd715140dad78fb73724b8f45ff4607410ba0097
brshnsk <brshnsk@gmail.com>
2022-10-05 18:43:23 -0400

    Agregando JoiValidation

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { isValidObjectId, Model } from 'mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
@@ -8,11 +9,16 @@ import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-
+  
+  private defaultLimit:number;
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+    
+    private readonly configService: ConfigService,
+  ){
+    this.defaultLimit = this.configService.get<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -29,7 +35,8 @@ export class PokemonService {
 
   findAll(paginationDto: PaginationDto) {
 
-    const {limit = 10, offset = 0} = paginationDto
+    
+    const {limit = this.defaultLimit, offset = 0} = paginationDto
 
     return this.pokemonModel.find()
       .limit(limit)
